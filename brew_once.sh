7f4a560627771c3f72162f310fee3f4ea900a94c
Mathi Fonseca <mathifonseca@gmail.com>
2021-11-18 10:48:09 -0500

    Update brew_once.sh

--- brew_once.sh
+++ brew_once.sh
@@ -9,64 +9,16 @@ brew upgrade
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
 ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-brew install binutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-brew install gnu-tar
-# Install a modern version of Bash.
 brew install bash
-brew install bash-completion2
-
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
-
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
-# Install more recent versions of some macOS tools.
-brew install git
-brew install curl
-brew install wget
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
-brew install php
-brew install gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install other system tools.
-brew install rename
-brew install tree
-brew install pstree
-brew install mas
-brew install htop
-brew install pidof
-brew install watch
-
-# Dev Tools
-brew install awscli
-brew install hub
 
 # Remove outdated versions from the cellar.
 brew cleanup
